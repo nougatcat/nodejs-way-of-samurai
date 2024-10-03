d304df532de5107a5ed4ffa2d6bed55990f432c4
nougatcat <105752568+nougatcat@users.noreply.github.com>
2024-10-04 00:32:57 +0300

    6 sync & async functions, filesystem (fs)

--- server.mjs
+++ server.mjs
@@ -1,29 +1,56 @@
 
 import http from 'http'; //Вариант для расширения .mjs
+import fs from 'fs'
 // const http = require('http') //Вариант для расширения .js
+// const fs = require('fs')
 
-let requestsCount = 0
+const delay = (ms) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, ms)
+    })
+} //Пример промисификации таймаута
+const readFile = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (error, data) => {
+            if (error) reject(error) 
+            else resolve(data)
+        })
+    })
+}
 
-const server = http.createServer((request, response) => {
-    requestsCount++
-    console.log(request.url)
-
+const server = http.createServer( async (request, response) => {
     switch (request.url) {
+        case '/about': {
+            await delay(3000)
+            response.write('about; delay 3000')
+            response.end()
+            break;
+        }
         case '/':
         case '/home':
-            response.write('home')
-            break;
-        case '/users':
-            response.write('users')
-            break;
-        case '/favicon.ico':  //чтобы в счетчик не записывался запрос на фавикон, а считывались только реальные заходы на сайт
-            requestsCount--
+            // const start = new Date()
+            // while(new Date() - start < 3000) { //? пример синхронной операции
+            //     console.log(new Date() - start)
+            // }
+            // setTimeout(() => { //? пример асинхронной операции 
+            //     const data = 'hello'
+            //     response.write(data)
+            //     response.end() 
+            // }, 3000)
+            try {
+                const data = await readFile('pages/about.html')
+                response.write(data)
+                response.end() 
+            }catch(error) { //обработка reject
+                response.write('error code 500')
+                response.end()
+            }
             break;
         default:
-            response.write('404')
+            response.write('error code 404')
+            response.end()
     }
-
-    response.write('\nHow many requests been since start ' + requestsCount)
-    response.end()
 })
 server.listen(3003)
