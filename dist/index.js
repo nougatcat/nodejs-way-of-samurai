2e2b6d4fde05e51fa4cc9f0c04881daf1f37798a
nougatcat <105752568+nougatcat@users.noreply.github.com>
2024-10-11 02:33:20 +0300

    express: get, post, delete, put

--- dist/index.js
+++ dist/index.js
@@ -1,67 +1,85 @@
 // import http from 'http'; //Вместо http теперь используем express
-import fs from 'fs';
+// import fs from 'fs';
 import express from 'express';
 const app = express();
 const port = 3003;
+const HTTP_STATUSES = {
+    OK_200: 200,
+    CREATED_201: 201,
+    NO_CONTENT_204: 204,
+    BAD_REQUEST_400: 400,
+    NOT_FOUND_404: 404
+};
+const db = {
+    courses: [
+        { id: 1, title: "front end 1" },
+        { id: 2, title: "devops 2" },
+        { id: 3, title: "backend" }
+    ]
+};
+//для парсинга body из post запросов
+const jsonBodyMiddleWare = express.json(); //не тот же метод джсон, что у респонсов
+app.use(jsonBodyMiddleWare);
 app.get('/', (req, res) => {
-    const a = 4;
-    if (a > 5)
-        res.send("ok");
-    else
-        res.send('Hello ooooowlrd');
+    res.send('<h1>Hello ooooowlrd</h1>'); //метод send автоматически понимает что там теги, если они есть 
 });
-app.get('/home', async (req, res) => {
-    try {
-        const data = await readFile('pages/home.html');
-        res.send(data); // тут предложит скачать файл, а не отобразит html как в методе http
+// для запросов типа /courses?title=end (query param) или /courses
+app.get('/courses', (req, res) => {
+    let foundCourses = db.courses;
+    if (req.query.title) {
+        foundCourses = foundCourses.filter(c => c.title.indexOf(req.query.title) > -1);
     }
-    catch (error) { //обработка reject
-        res.send('error code 500');
+    // if (!foundCourses) { //не нужно выдавать 404, достаточно, если вернется пустой массив
+    //     res.sendStatus(404)
+    //     return;
+    // }
+    res.json(foundCourses);
+});
+// для запросов типа /courses/1 (uri param)
+app.get('/courses/:id', (req, res) => {
+    const foundCourse = (db.courses.find(c => c.id === +req.params.id));
+    if (!foundCourse) {
+        res.sendStatus(404);
+        return;
     }
+    res.json(foundCourse);
+});
+app.post('/courses', (req, res) => {
+    if (!req.body.title || !req.body.title.trim()) { //req.body.title.trim() нужен для проверки, не прислали ли строку только с пробелами
+        res.sendStatus(400); //validation error
+        return;
+    }
+    const createdCourse = {
+        id: +(new Date()), //дата переведенная в number, дата - это рандомное число для примера вместо нормальной генерации id
+        title: req.body.title
+    };
+    db.courses.push(createdCourse);
+    res.status(201).json(createdCourse);
+}); //такой POST сохранится до перезагрузки сервера, потому что нет БД
+app.delete('/courses/:id', (req, res) => {
+    db.courses = db.courses.filter(c => c.id !== +req.params.id);
+    res.sendStatus(204); //no content
+});
+app.put('/courses/:id', (req, res) => {
+    if (!req.body.title || !req.body.title.trim()) { //req.body.title.trim() нужен для проверки, не прислали ли строку только с пробелами
+        res.sendStatus(400); //validation error
+        return;
+    }
+    const foundCourse = (db.courses.find(c => c.id === +req.params.id));
+    if (!foundCourse) {
+        res.sendStatus(404);
+        return;
+    }
+    foundCourse.title = req.body.title;
+    res.sendStatus(204);
 });
 app.listen(port, () => {
     console.log(`the server is running on port ${port}`);
 });
-// const delay = (ms) => {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve()
-//         }, ms)
-//     })
-// } //Пример промисификации таймаута
-const readFile = (path) => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, (error, data) => {
-            if (error)
-                reject(error);
-            else
-                resolve(data);
-        });
-    });
-};
-// const server = http.createServer( async (request, response) => {
-//     switch (request.url) {
-//         case '/about': {
-//             await delay(3000)
-//             response.write('about; delay 3000')
-//             response.end()
-//             break;
-//         }
-//         case '/':
-//         case '/home':
-//             try {
-//                 const data = await readFile('pages/about.html')
-//                 response.write(data)
-//                 response.end()
-//             }catch(error) { //обработка reject
-//                 response.write('error code 500')
-//                 response.end()
-//             }
-//             break;
-//         default:
-//             response.write('error code 404')
-//             response.end()
-//     }
+// app.get('/home', async (req, res) => {
+// try {
+//     const data = await readFile('pages/home.html')
+//     res.send(data) // тут предложит скачать файл, а не отобразит html как в методе http
+// } catch (error) { //обработка reject
+//     res.send('error code 500')
 // })
-// server.listen(3003)
-// console.log('the server is running on port 3003')
